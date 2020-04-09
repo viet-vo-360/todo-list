d9b3b06b3f8e00d26e7654ab59d0a617a68f009e
viet.vo <viet.vo@coxautoinc.com>
2020-04-09 16:29:15 +0700

    Update pagination config

--- src/App.js
+++ src/App.js
@@ -14,11 +14,21 @@ export const TodoContext = createContext();
 
 const App = () => {
   const [todos, dispatchTodos] = useReducer(todoReducer, FORM_INITIAL_STATE);
+  const pageSize = 5;
+  const currentPage = Math.ceil(todos.length / pageSize);
+  const tablePagination = {
+    pageSizeOptions: ['5', '10', '15'],
+    defaultPageSize: pageSize,
+    current: currentPage,
+    showSizeChanger: true,
+    showQuickJumper: true
+  };
+
   return (
     <TodoContext.Provider value={[todos, dispatchTodos]}>
       <Row type="flex" justify="center">
         <Col xs={24} sm={24} md={24} lg={20} xl={20}>
-          <Table dataSource={todos} columns={FORM_COLUMNS} pagination={{ pageSize: 5 }} />
+          <Table dataSource={todos} columns={FORM_COLUMNS} pagination={tablePagination} />
         </Col>
       </Row>
       <Row type="flex" justify="center">
