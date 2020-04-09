009a2f4c5f7b5ab2790b1b301db3c460194220b3
viet.vo <viet.vo@coxautoinc.com>
2020-04-09 13:41:53 +0700

    init tasks

--- src/utils/constants/INITIAL_STATE.js
+++ src/utils/constants/INITIAL_STATE.js
@@ -3,10 +3,38 @@ import { v4 as uuidv4 } from 'uuid';
 export const FORM_INITIAL_STATE = [
   {
     key: uuidv4(),
-    title: 'Use Hooks in a React application (Default TODO from STATE)',
+    title: 'Do Housecleaning',
     date: '2020-04-15',
     category: 'Work',
     completed: 'false',
     dataIndex: 0,
   },
+  {
+    key: uuidv4(),
+    title: 'Organize party with Hurricane team',
+    date: '2020-04-16',
+    completed: 'false',
+    dataIndex: 0,
+  },
+  {
+    key: uuidv4(),
+    title: 'Go study English at the Center',
+    date: '2020-04-14',
+    completed: 'false',
+    dataIndex: 0,
+  },
+  {
+    key: uuidv4(),
+    title: 'Have an appointment with a doctor at the hospital',
+    date: '2020-04-20',
+    completed: 'false',
+    dataIndex: 0,
+  },
+  {
+    key: uuidv4(),
+    title: 'Go to the supermarket',
+    date: '2020-04-20',
+    completed: 'false',
+    dataIndex: 0,
+  }
 ];
