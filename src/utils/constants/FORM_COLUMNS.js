0d824e6c844c3ea305233a169d024b29ff5584a2
An-Nguyen <andqnguyen@kms-technology.com>
2020-04-11 11:36:13 +0700

    Revert "Update Form Columns"
    This reverts commit 3d66331ebaa7d7abba1f732282df979499c47571.

--- src/utils/constants/FORM_COLUMNS.js
+++ src/utils/constants/FORM_COLUMNS.js
@@ -3,19 +3,8 @@ import React from 'react';
 import { Complete } from '../../components/Complete/Complete.component';
 import { Delete } from '../../components/Delete/Delete.component';
 import { Title } from '../../components/Title/Title.component';
-import { Checkbox } from '../../components/CheckBox/CheckBox.component';
-import { Checkbox as CheckBoxAll} from "antd";
 
 export const FORM_COLUMNS = [
-  {
-    title: <CheckBoxAll className="ckb-all"> Check All</CheckBoxAll>,
-    dataIndex: 'checkbox',
-    key: 'checkbox',
-    render: (record) => {
-      return <Checkbox record={record}></Checkbox>;
-    },
-  },
-
   {
     title: 'Title',
     dataIndex: 'title',
@@ -47,7 +36,7 @@ export const FORM_COLUMNS = [
     title: 'Action',
     key: 'action',
     dataIndex: 'action',
-    render: (record) => {
+    render: (text, record) => {
       return (
         <>
           <Complete record={record} />
