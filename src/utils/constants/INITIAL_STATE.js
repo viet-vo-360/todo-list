504e2ed21de291a89244d96c9c96e170aed8bd03
An-Nguyen <andqnguyen@kms-technology.com>
2020-04-11 11:37:42 +0700

    Revert this change

--- src/utils/constants/INITIAL_STATE.js
+++ src/utils/constants/INITIAL_STATE.js
@@ -1,26 +1,20 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 export const FORM_INITIAL_STATE = [
   {
     key: uuidv4(),
-    title: "Do Housecleaning",
-    date: "2020-04-15",
-    category: "Work",
-    completed: "false",
+    title: 'Do Housecleaning',
+    date: '2020-04-15',
+    category: 'Work',
+    completed: 'false',
     dataIndex: 0,
-    checkbox: {
-      isChecked: false,
-    },
   },
   {
     key: uuidv4(),
-    title: "Go to the supermarket",
-    date: "2020-04-20",
-    completed: "false",
-    category: "Shopping",
+    title: 'Go to the supermarket',
+    date: '2020-04-20',
+    completed: 'false',
+    category: 'Shopping',
     dataIndex: 0,
-    checkbox: {
-      isChecked: true,
-    },
-  },
+  }
 ];
