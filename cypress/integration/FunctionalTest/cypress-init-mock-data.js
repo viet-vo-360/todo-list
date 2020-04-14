d276c6cc31f6ae6972df61e8f30702e1b1e13ce1
An-Nguyen <andqnguyen@kms-technology.com>
2020-04-14 10:34:01 +0700

    Update cypress test

--- cypress/integration/FunctionalTest/cypress-init-mock-data.js
+++ cypress/integration/FunctionalTest/cypress-init-mock-data.js
@@ -1,15 +1,25 @@
 /* eslint-disable no-undef */
 const mockData = require("../../fixtures/todos.json");
 describe("Add todo task", () => {
-  it('it should use mock data', function () {
-    cy.server()
-    cy.fixture('todos.json').as('mockData');
-    cy.route('http://me.todolist.com/todos/list', '@mockData').as('fetchData')
-    cy.visit('/', {
-      onBeforeLoad (win) {
-        delete win.fetch
+  it("it should use mock data", function () {
+    cy.server();
+    cy.fixture("todos.json").as("mockData");
+    cy.route("http://me.todolist.com/todos/list", "@mockData").as("fetchData");
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        delete win.fetch;
       },
-    })
-    cy.wait('@fetchData');
-  })
+    });
+    cy.wait("@fetchData");
+  });
+  it("it should use mock data", function () {
+    cy.visit("/");
+
+    cy.window().then((win) => {
+      cy.url().should("eq", win.location.href);
+    });
+    cy.document().then((doc) => {
+      doc.body.style.background = "red";
+    });
+  });
 });
